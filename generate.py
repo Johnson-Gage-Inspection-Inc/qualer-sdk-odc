f8fbce09d1d454bf5a7224a21b4c557e0aa4cb4b
Jeffrey Hall <75549255+rhythmatician@users.noreply.github.com>
2025-04-02 03:42:15 -0500

    Refactor ODC XML generation for improved readability and structure

--- generate.py
+++ generate.py
@@ -26,27 +26,42 @@ def generate_odc_file(group, name, url, parameters):
 
     mashup_formula = "let\n    " + "\n    ".join(mashup_lines)
 
-    odc_xml = f"""<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns="http://www.w3.org/TR/REC-html40">
+    odc_xml = f"""<html xmlns:o="urn:schemas-microsoft-com:office:office"
+xmlns="http://www.w3.org/TR/REC-html40">
+
 <head>
 <meta http-equiv=Content-Type content="text/x-ms-odc; charset=utf-8">
 <meta name=ProgId content=ODC.Database>
 <meta name=SourceType content=OLEDB>
 <title>{name}</title>
-<xml id=docprops><o:DocumentProperties xmlns:o="urn:schemas-microsoft-com:office:office" xmlns="http://www.w3.org/TR/REC-html40">
+<xml id=docprops><o:DocumentProperties
+  xmlns:o="urn:schemas-microsoft-com:office:office"
+  xmlns="http://www.w3.org/TR/REC-html40">
   <o:Description>Connection to the '{name}' query in the workbook.</o:Description>
   <o:Name>{name}</o:Name>
-</o:DocumentProperties></xml>
-<xml id=msodc><odc:OfficeDataConnection xmlns:odc="urn:schemas-microsoft-com:office:odc" xmlns="http://www.w3.org/TR/REC-html40">
+ </o:DocumentProperties>
+</xml><xml id=msodc><odc:OfficeDataConnection
+  xmlns:odc="urn:schemas-microsoft-com:office:odc"
+  xmlns="http://www.w3.org/TR/REC-html40">
   <odc:PowerQueryConnection odc:Type="OLEDB">
    <odc:ConnectionString>Provider=Microsoft.Mashup.OleDb.1;Data Source=$Workbook$;Location={name};Extended Properties=&quot;&quot;</odc:ConnectionString>
    <odc:CommandType>SQL</odc:CommandType>
    <odc:CommandText>SELECT * FROM [{name}]</odc:CommandText>
   </odc:PowerQueryConnection>
-  <odc:PowerQueryMashupData>&lt;Mashup xmlns:xsd=&quot;http://www.w3.org/2001/XMLSchema&quot; xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot; xmlns=&quot;http://schemas.microsoft.com/DataMashup&quot;&gt;&lt;Client&gt;EXCEL&lt;/Client&gt;&lt;Version&gt;2.140.405.0&lt;/Version&gt;&lt;MinVersion&gt;2.21.0.0&lt;/MinVersion&gt;&lt;Culture&gt;en-US&lt;/Culture&gt;&lt;SafeCombine&gt;true&lt;/SafeCombine&gt;&lt;Items&gt;&lt;Query Name=&quot;{name}&quot;&gt;&lt;Formula&gt;&lt;![CDATA[{mashup_formula}]]&gt;&lt;/Formula&gt;&lt;/Query&gt;&lt;/Items&gt;&lt;/Mashup&gt;</odc:PowerQueryMashupData>
-</odc:OfficeDataConnection></xml>
-</head>
-<body></body>
-</html>"""
+  <odc:PowerQueryMashupData>
+    &lt;Mashup xmlns:xsd=&quot;http://www.w3.org/2001/XMLSchema&quot; xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot; xmlns=&quot;http://schemas.microsoft.com/DataMashup&quot;&gt;&lt;Client&gt;EXCEL&lt;/Client&gt;&lt;Version&gt;2.140.405.0&lt;/Version&gt;&lt;MinVersion&gt;2.21.0.0&lt;/MinVersion&gt;&lt;Culture&gt;en-US&lt;/Culture&gt;&lt;SafeCombine&gt;true&lt;/SafeCombine&gt;&lt;Items&gt;&lt;Query Name=&quot;{name}&quot;&gt;&lt;Formula&gt;&lt;![CDATA[{mashup_formula}]]&gt;&lt;/Formula&gt;&lt;IsParameterQuery xsi:nil=&quot;true&quot; /&gt;&lt;IsDirectQuery xsi:nil=&quot;true&quot; /&gt;&lt;/Query&gt;&lt;/Items&gt;&lt;/Mashup&gt;
+ </odc:PowerQueryMashupData>
+ </odc:OfficeDataConnection>
+</xml>
+""" + """
+<style>
+<!--
+    .ODCDataSource
+    {
+    behavior: url(dataconn.htc);
+    }
+-->
+</style>"""
 
     folder = OUTPUT_DIR / group
     folder.mkdir(parents=True, exist_ok=True)
