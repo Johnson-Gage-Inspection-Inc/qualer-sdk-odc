8564f232731c23e1c31439b5f0f2fc5980339911
Jeffrey Hall <75549255+rhythmatician@users.noreply.github.com>
2025-04-03 23:54:43 -0500

    Add render_odc_xml function to generate ODC XML for queries

--- generate.py
+++ generate.py
@@ -3,6 +3,7 @@ from doc_writer import generate_markdown_file, generate_docs_index
 from pathlib import Path
 import json
 import re
+from odc_writer import render_odc_xml
 
 # === Config ===
 SWAGGER_URL = "https://jgiquality.qualer.com/swagger/docs/v1"
@@ -62,45 +63,7 @@ def generate_odc_file(ep):
 
     mashup_formula = "let\n    " + "\n    ".join(mashup_lines)
 
-    odc_xml = (
-        f"""<html xmlns:o="urn:schemas-microsoft-com:office:office"
-xmlns="http://www.w3.org/TR/REC-html40">
-
-<head>
-<meta http-equiv=Content-Type content="text/x-ms-odc; charset=utf-8">
-<meta name=ProgId content=ODC.Database>
-<meta name=SourceType content=OLEDB>
-<title>Query - {name}</title>
-<xml id=docprops><o:DocumentProperties
-  xmlns:o="urn:schemas-microsoft-com:office:office"
-  xmlns="http://www.w3.org/TR/REC-html40">
-  <o:Description>Connection to the '{name}' query in the workbook.</o:Description>
-  <o:Name>Query - {name}</o:Name>
- </o:DocumentProperties>
-</xml><xml id=msodc><odc:OfficeDataConnection
-  xmlns:odc="urn:schemas-microsoft-com:office:odc"
-  xmlns="http://www.w3.org/TR/REC-html40">
-  <odc:PowerQueryConnection odc:Type="OLEDB">
-   <odc:ConnectionString>Provider=Microsoft.Mashup.OleDb.1;Data Source=$Workbook$;Location={name};Extended Properties=&quot;&quot;</odc:ConnectionString>
-   <odc:CommandType>SQL</odc:CommandType>
-   <odc:CommandText>SELECT * FROM [{name}]</odc:CommandText>
-  </odc:PowerQueryConnection>
-  <odc:PowerQueryMashupData>
-    &lt;Mashup xmlns:xsd=&quot;http://www.w3.org/2001/XMLSchema&quot; xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot; xmlns=&quot;http://schemas.microsoft.com/DataMashup&quot;&gt;&lt;Client&gt;EXCEL&lt;/Client&gt;&lt;Version&gt;2.140.405.0&lt;/Version&gt;&lt;MinVersion&gt;2.21.0.0&lt;/MinVersion&gt;&lt;Culture&gt;en-US&lt;/Culture&gt;&lt;SafeCombine&gt;true&lt;/SafeCombine&gt;&lt;Items&gt;&lt;Query Name=&quot;{name}&quot;&gt;&lt;Formula&gt;&lt;![CDATA[{mashup_formula}]]&gt;&lt;/Formula&gt;&lt;IsParameterQuery xsi:nil=&quot;true&quot; /&gt;&lt;IsDirectQuery xsi:nil=&quot;true&quot; /&gt;&lt;/Query&gt;&lt;/Items&gt;&lt;/Mashup&gt;
- </odc:PowerQueryMashupData>
- </odc:OfficeDataConnection>
-</xml>
-"""
-        + """
-<style>
-<!--
-    .ODCDataSource
-    {
-    behavior: url(dataconn.htc);
-    }
--->
-</style>"""
-    )
+    odc_xml = render_odc_xml(name, mashup_formula)
 
     folder = OUTPUT_DIR / ep["tag"]
     folder.mkdir(parents=True, exist_ok=True)
